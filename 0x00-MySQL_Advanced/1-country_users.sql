8dbf577ba7b095064cc6c5b2c9ffb80f21d2eae5
Idouahman Aymen <aymanidou@gmail.com>
2024-03-21 10:28:32 +0000

    1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -10,4 +10,5 @@ CREATE TABLE IF NOT EXISTS users (
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255),
   country VARCHAR(2) NOT NULL DEFAULT 'US',
+  CHECK (country IN ('US', 'CO', 'TN'))
 );

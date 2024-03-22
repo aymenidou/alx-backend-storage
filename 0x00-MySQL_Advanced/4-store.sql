080745c5b2fca140e27e866eb7fdaf9f6cbe4258
Idouahman Aymen <aymanidou@gmail.com>
2024-03-22 18:45:28 +0000

    4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,17 +1,18 @@
 -- a trigger that decreases the quantity of an item after adding a new order.
-DROP trigger IF exist update_item_quantity;
+DROP TRIGGER IF EXIST update_item_quantity;
 
 DELIMITER $$
 CREATE TRIGGER update_item_quantity
 AFTER
 INSERT
-  ON orders FOR EACH ROW BEGIN
+  ON orders FOR EACH ROW 
+BEGIN
 UPDATE
   items
 SET
   quantity = quantity - NEW.number
 WHERE
-  items.name = NEW.item_name;
+  name = NEW.item_name;
 
 END $$
 DELIMITER ;

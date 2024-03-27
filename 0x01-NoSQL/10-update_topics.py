24c917612ab5e588ec13dafb584202dd27b69ca2
Aymen Idouahman <aymanidou@gmail.com>
2024-03-27 10:40:03 +0000

    10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,14 +3,14 @@
 
 
 def update_topics(mongo_collection, name, topics):
-    """
+    '''
     Updates the topics field of a school document based on the name.
 
     Args:
         mongo_collection: A pymongo collection object.
         name: The name of the school document to update (string).
         topics: The new list of topics to be assigned (list of strings).
-    """
+    '''
     filter = {"name": name}
     update = {"$set": {"topics": topics}}
     mongo_collection.update_one(filter, update)

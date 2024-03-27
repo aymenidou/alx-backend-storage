ebbfcaef4aa3ef68b0414ae830dc2d12e86180d8
Aymen Idouahman <aymanidou@gmail.com>
2024-03-27 10:37:44 +0000

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,7 +3,7 @@
 
 
 def list_all(mongo_collection):
-    """
+    '''
     Fetches all documents from a MongoDB collection.
 
     Args:
@@ -12,6 +12,6 @@ def list_all(mongo_collection):
     Returns:
         A list of dictionaries representing the documents in the collection.
         An empty list if no documents are found.
-    """
+    '''
     documents = list(mongo_collection.find())
     return documents

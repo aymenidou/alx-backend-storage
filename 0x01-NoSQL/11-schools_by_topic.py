435d3c2e3205d771355b3d1f9a29aaba56d44284
Aymen Idouahman <aymanidou@gmail.com>
2024-03-27 01:30:05 +0000

    11-schools_by_topic.py

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns a list of school documents that have a specific topic.
+
+    Args:
+        mongo_collection: A pymongo collection object.
+        topic: The topic to search for (string).
+
+    Returns:
+        A list of school documents matching the search criteria.
+    """
+    filter = {"topics": {"$in": [topic]}}  # Use $in operator for list search
+    return list(mongo_collection.find(filter))

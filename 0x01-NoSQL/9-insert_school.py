5d321c9903139f624e8ba2ecce65f934296be79c
Aymen Idouahman <aymanidou@gmail.com>
2024-03-27 01:25:54 +0000

    9-insert_school.py

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def insert_school(mongo_collection, **kwargs):
+    '''
+    Inserts a new document into a MongoDB collection using keyword arguments.
+
+    Args:
+        mongo_collection: A pymongo collection object.
+        **kwargs: Keyword arguments representing the document fields.
+
+    Returns:
+        The ObjectId of the inserted document.
+    '''
+    new_document = mongo_collection.insert_one(kwargs)
+    return new_document.inserted_id

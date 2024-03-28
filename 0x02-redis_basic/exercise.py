a8d10a2b161d6a2f2df324c6f649620719bc61d4
Aymen Idouahman <aymanidou@gmail.com>
2024-03-28 01:11:19 +0000

    exercise.py 0. Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 '''0x02-redis_basic'''
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Any
 
 
 class Cache:
@@ -19,3 +19,19 @@ class Cache:
         key = str(uuid.uuid4())  # Generate a unique key
         self._redis.set(key, data)  # Store the data in Redis
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Any:
+        """Retrieves data from Redis by key, optionally applying
+          a conversion function."""
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)  # Apply conversion if a function is provided
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Retrieves data as a string."""
+        return self.get(key, fn=str)
+
+    def get_int(self, key: str) -> int:
+        """Retrieves data as an integer."""
+        return self.get(key, fn=int)
